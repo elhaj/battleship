8c1525dbb61826fb5dc65fca7e4209595f5d594e
Sidi Mohamed El Haj Sidi <sidimohamedelhajsidi@Sidis-MacBook-Pro.local>
2019-05-02 00:06:23 -0700

    Add logic to read the id from the path

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 const AWS = require('aws-sdk');
 
 exports.handler = (event, ctx, callback) => {
-    var params = {
+    const id = event.pathParameters['ship-id'];
+    const params = {
         TableName: 'Test',
         Key: {
-            Id: '1'
+            Id: id
         }
     };
-    var documentClient = new AWS.DynamoDB.DocumentClient();
+    const documentClient = new AWS.DynamoDB.DocumentClient();
 
     documentClient.get(params, function (err, data) {
         if (err) console.log(err);
-        else callback(undefined, data);
+
+        const response = {
+            "statusCode": 200,
+            "body": JSON.stringify(data)
+        };
+        callback(undefined, response);
     });
 };
